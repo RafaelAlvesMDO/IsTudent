ef21ad487e9627c3d089a1bdeca584c0d3a35b9c
Rafael <rafaelalvesmdo_oficial@hotmail.com>
2025-04-04 19:56:31 -0300

    Modifications Done

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\TemplateController;
+use App\Http\Controllers\RoomsController;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+// Route::get('/', function () {
+//     return Inertia::render('Welcome');
+// })->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
@@ -13,3 +15,8 @@ Route::get('dashboard', function () {
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
+
+Route::get('/template', [TemplateController::class, 'index']);
+Route::get('/', [RoomsController::class, 'list']);
+Route::get('/rooms/{Name}', [RoomsController::class, 'detail']);
+
